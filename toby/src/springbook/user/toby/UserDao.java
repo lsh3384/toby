efba84b9dbc143250dbad03799f306c5c07a7ecc
lee <lsh3384@gmail.com>
2022-03-23 23:20:46 +0900

    class의 분리
    관심사를 분리하기 위해서 db연결이라는 관심을 가진 부분을 SimpleConnectionMaker로 분리함

--- toby/src/springbook/user/toby/UserDao.java
+++ toby/src/springbook/user/toby/UserDao.java
@@ -6,9 +6,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public abstract class UserDao {
+public class UserDao {
+	private SimpleConnectionMaker simpleConnectionMaker;
+	
+	public UserDao() {
+		simpleConnectionMaker = new SimpleConnectionMaker();
+	}
+	
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Connection c = getConnection();
+		Connection c = simpleConnectionMaker.makeNewConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1, user.getId());
@@ -22,7 +28,7 @@ public abstract class UserDao {
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = getConnection();
+		Connection c = simpleConnectionMaker.makeNewConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1,  id);
@@ -42,10 +48,8 @@ public abstract class UserDao {
 	}
 	
 	
-	public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		NUserDao dao = new NUserDao();
+		UserDao dao = new UserDao();
 		
 		User user = new User();
 		user.setId("whiteship");
