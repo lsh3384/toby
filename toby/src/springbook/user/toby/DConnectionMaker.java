e9f3a6acd134ec06796bd1a075fe412c1580bd93
lee <lsh3384@gmail.com>
2022-03-23 23:38:07 +0900

    1.3.2 인터페이스의 도입

--- toby/src/springbook/user/toby/DConnectionMaker.java
+++ toby/src/springbook/user/toby/DConnectionMaker.java
@@ -4,11 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
+public class DConnectionMaker implements ConnectionMaker {
 	public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "root", "toby1234");
-		
+
 		return c;
 	}
 }
