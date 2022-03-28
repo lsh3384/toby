95ea5c3c0241616cc8e44de588abaebb8bff4dc4
lee <lsh3384@gmail.com>
2022-03-28 15:01:29 +0900

    spring 반영중

--- src/main/java/springbook/user/toby/DaoFactory.java
+++ src/main/java/springbook/user/toby/DaoFactory.java
@@ -3,6 +3,7 @@ package springbook.user.toby;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// spring반영중
 @Configuration
 public class DaoFactory {
 	@Bean
