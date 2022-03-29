7fb8bbb6716b22b2a9c8c27ffec1c30d8ab2a771
lee <lsh3384@gmail.com>
2022-03-29 16:20:36 +0900

    delete

--- toby/springbook/user/toby/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package toby.springbook.user.toby;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-	public Connection makeNewConnection() throws ClassNotFoundException, SQLException;
-}
